a363267947d5ff2588c708147f6821bc87654acc
The Natural <philip.smalls86@gmail.com>
2019-06-02 17:15:24 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -13,4 +13,9 @@ def happy_birthday(birthday_kids)
 end
 
 def age_appropriate_birthday()
+  birthday_kids.each do |name, age|
+    if age <= 12 
+      puts "Happy Birthday #{name}! You are now #{age} years old!"
+    end 
+  end 
 end 
