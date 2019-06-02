2b5c32edaab2d878857b963df3ea7d924cb859c0
The Natural <philip.smalls86@gmail.com>
2019-06-02 17:09:25 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -6,6 +6,10 @@
 
 def happy_birthday(birthday_kids)
   # add your code snippet here!
+  
+  birthday_kids.each do |name, age|
+    puts "Happy Birthday #{name}! Youo are now #{age} years old!"
+  end 
 end
 
 
